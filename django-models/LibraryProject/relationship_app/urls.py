8bc852348a689bb1de38a3b191b48c05270a89ce
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-12-01 18:37:18 +0200

    Task3 changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,23 +1,20 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views  # Make sure this imports your views.py
+from . import views
 
 urlpatterns = [
-    # Book & Library views
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Authentication
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 
-    # RBAC views
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    # Permission-protected Book views
+    # **Permission-protected Book views**
     path('book/add/', views.add_book, name='add_book'),
     path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
