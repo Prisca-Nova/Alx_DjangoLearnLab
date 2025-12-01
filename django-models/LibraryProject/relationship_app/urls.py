b6818789c1dcfc1cf9e178d38979744b586926e3
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-12-01 18:33:01 +0200

    Task3 changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    # Existing views...
+    # Existing views
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
@@ -13,7 +13,7 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    # Secured book actions
+    # Permission-protected Book views
     path('book/add/', views.add_book, name='add_book'),
     path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
