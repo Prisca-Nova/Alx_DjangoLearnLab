33709cc182ceb8876a1ea065f416b7f6b1c6fabf
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-12-01 17:01:51 +0200

    Task2 changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.generic.detail import DetailView  # Already required by previous task
+from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm  
 from django.contrib.auth.decorators import login_required
 from .models import Book
-from .models import Library  # Separate import required by checker
-
+from .models import Library  
 # --- Existing views ---
 def list_books(request):
     books = Book.objects.all()
@@ -38,7 +38,7 @@ def register_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # auto-login after registration
+            login(request, user)
             return redirect('list_books')
     else:
         form = UserCreationForm()
