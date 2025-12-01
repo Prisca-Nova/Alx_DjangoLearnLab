977777b9e970f84c4d7e019ddfb9ffe47fc4e0ce
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-12-01 16:48:12 +0200

    Task1 changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView  
 from .models import Book
 from .models import Library 
+
 # Function-based view: list all books with title and author
 def list_books(request):
     books = Book.objects.all()
