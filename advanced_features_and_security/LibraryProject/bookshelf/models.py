77cd2bc9dce63d0513dfa5b4fcdd186c06634451
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-12-01 21:50:32 +0200

    Task0 changes advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,48 +1,10 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
-
-class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField(_('email address'), unique=True)
-    date_of_birth = models.DateField(_('date of birth'), null=True, blank=True)
-    profile_photo = models.ImageField(
-        _('profile photo'), 
-        upload_to='profile_photos/',
-        null=True, 
-        blank=True
-    )
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-    
-    objects = CustomUserManager()
-    
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-    
     def __str__(self):
-        return self.email
+        return self.title
--- advanced_features_and_security/LibraryProject/bookshelf/settings.py
+++ advanced_features_and_security/LibraryProject/bookshelf/settings.py
@@ -1,13 +1,5 @@
-# Add this line
-AUTH_USER_MODEL = 'bookshelf.CustomUser'
-
-# Make sure bookshelf is in INSTALLED_APPS
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'bookshelf',  # This line must be present
+    # other apps
+    'accounts',
+    'bookshelf',
 ]
