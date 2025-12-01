4a0274b7e758c8de84de656dfcf29132f6c79e69
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-12-01 17:45:40 +0200

    Task3 changes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -18,4 +18,4 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance, role='Member')  # Default role
+        UserProfile.objects.create(user=instance, role='Member')
