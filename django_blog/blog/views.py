cba470b8c21c3b8290b8054658e48e35fad3b1d7
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-12-02 17:02:06 +0200

    Add blog post management: implement forms, views, and URLs for creating, updating, deleting, and listing posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from .forms import CustomUserCreationForm, PostForm
+from .models import Post
 from django.contrib import messages
 
 def register(request):
@@ -34,3 +38,46 @@ def profile(request):
         return redirect("blog:profile")
 
     return render(request, "blog/profile.html", {"user": user})
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:post-detail', kwargs={'pk': self.object.pk})
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+    def get_success_url(self):
+        return reverse('blog:post-detail', kwargs={'pk': self.object.pk})
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy('blog:post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
